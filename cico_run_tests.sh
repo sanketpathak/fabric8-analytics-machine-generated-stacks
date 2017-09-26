2984922604cd229c8c9cc3916c1700b32b5f4ffe
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-09-26 11:45:08 +0530

    initial commit with sample codebase and files requried for CICD

---
+++ cico_run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+. cico_setup.sh
+
+install_dependencies
+
+run_unit_tests
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+// The usual bootstrapping imports
+
+import './polyfills';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
