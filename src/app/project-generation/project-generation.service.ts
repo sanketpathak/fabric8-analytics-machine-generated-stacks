3551c69e952d637f5b76a78620991525b6988aff
sanketpathak <spathak@redhat.com>
2017-09-29 17:22:16 +0530

    Functionality to autodownload the required pom.xml file

--- src/app/project-generation/project-generation.service.ts
+++ src/app/project-generation/project-generation.service.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/map';
 
 export class ProjectGenerationService {
     public service: Observable<any>;
-    private headers: Headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+    private headers: Headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(
         private http: Http,
@@ -29,6 +29,23 @@ export class ProjectGenerationService {
                                 .catch(this.handleError);
 
         return this.service;
+    }
+
+    testRoute(): void {
+        let url: string = 'http://10.209.69.58:5000/test';
+        let body: any = JSON.stringify({'name': 'Some Name'});
+        
+        let options = new RequestOptions({ headers: this.headers });
+        this.service = this     .http
+                                .post(url, body, options)
+                                .map((response: Response) => {
+                                    console.log(response);
+                                    var blob = new Blob([response], { type: 'text/xml' });
+                                    var url= window.URL.createObjectURL(blob);
+                                    window.open(url);
+                                })
+                                .catch(this.handleError);
+
     }
 
     postJSON() {
@@ -55,11 +72,24 @@ export class ProjectGenerationService {
         this.headers.set('Accept', 'multipart/form-data');
         let options = new RequestOptions({ headers: this.headers });
         let body: any = JSON.stringify(postData);
-        let url: string = '';
+        let url: string = 'http://10.209.69.58:5000/generatefile';
         this.service = this     .http
                                 .post(url, body, options)
-                                .map(this.extractData)
+                                .map((response: any) => {
+                                    console.log(response);
+                                    let blob = new Blob([response._body], { type: 'application/download' });
+                                    let url= window.URL.createObjectURL(blob);
+                                    let a : any = document.createElement('a');
+                                    
+                                    a.href = url;
+                                    a.download = "pom.xml" ;
+                                    document.body.appendChild(a);
+                                    a.click();
+                                    // window.open(url);
+                                })
                                 .catch(this.handleError);
+
+        //this.testRoute();
 
         return this.service; 
     }
