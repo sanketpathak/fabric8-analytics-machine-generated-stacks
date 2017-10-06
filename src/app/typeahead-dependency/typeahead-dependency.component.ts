f55c9767d882d6cfa85a3c71390279b79aeb2ec5
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-10-06 17:12:23 +0530

    temporary push

--- src/app/typeahead-dependency/typeahead-dependency.component.ts
+++ src/app/typeahead-dependency/typeahead-dependency.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, Output, OnChanges, EventEmitter } from '@angular/core';
 import { TypeAheadDependencyService } from './typeahead-dependency.service';
+import { ComponentAnalysisService } from '../component-analysis/component-analysis.service';
 import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'typeahead-dependency',
     templateUrl: './typeahead-dependency.component.html',
     styleUrls: ['./typeahead-dependency.component.scss'],
-    providers: [TypeAheadDependencyService]
+    providers: [TypeAheadDependencyService, ComponentAnalysisService]
 })
 
 export class TypeAheadDependencyComponent {
@@ -18,7 +19,62 @@ export class TypeAheadDependencyComponent {
     public isLoading: boolean = false;
     private subscription: Subscription;
 
-    constructor(private typeAheadDependencyService: TypeAheadDependencyService) {}
+    @Output() onAnalyze = new EventEmitter();
+    @Output() onOut = new EventEmitter();
+    private subscriptionComponent: Subscription;
+    public componentAnalysis: any = {};
+    private cache: any = {};
+    public showDetail: boolean = false;
+
+    constructor(private typeAheadDependencyService: TypeAheadDependencyService,
+                private componentAnalysisService: ComponentAnalysisService) {}
+
+    /** Address the redundancy */
+    public handleMouseOut(): void {
+        this.onOut.emit(true);
+    }
+
+    public showComponentAnalysis(component: any, comp: Element): void {
+        if (component) {
+            if (this.subscriptionComponent) {
+                this.subscriptionComponent.unsubscribe();
+            }
+            if (this.cache && this.cache[component.name] !== undefined) {
+                this.componentAnalysis = this.cache[component.name];
+                this.showDetail = true;
+                this.onAnalyze.emit({
+                    content: this.componentAnalysis,
+                    ref: comp
+                });
+            } else {
+                this.subscriptionComponent = this    .componentAnalysisService
+                                            .getComponentAnalysis(component)
+                                            .subscribe(response => {
+                                                if (response) {
+                                                    console.log(response);
+                                                    debugger;
+                                                    let pack: any = response.result.data[0].package;
+                                                    let github: any = {};
+                                                    github['forks'] = pack['gh_forks'] ? pack['gh_forks'][0] : 'NA';
+                                                    github['issues'] = pack['gh_open_issues_count'] ? pack['gh_open_issues_count'][0] : 'NA';
+                                                    github['stars'] = pack['gh_stargazers'] ? pack['gh_stargazers'][0] : 'NA';
+                                                    github['subscribes'] = pack['gh_subscribers_count'] ? pack['gh_subscribers_count'][0] : 'NA';
+
+                                                    this.componentAnalysis['github'] = github;
+                                                    this.componentAnalysis['latest_version'] = pack['latest_version'] ? pack['latest_version'][0] : 'NA';
+                                                    this.componentAnalysis['tokens'] = pack['tokens'];
+                                                    this.cache[component.name] = this.componentAnalysis;
+                                                    this.onAnalyze.emit({
+                                                        content: this.componentAnalysis,
+                                                        ref: comp
+                                                    });
+                                                }
+                                                this.showDetail = true;
+                                            });
+            }
+        }
+    }
+    /** Address the redundancy */
 
     onInputChange(): void {
         if (this.subscription) {
